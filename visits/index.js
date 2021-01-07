dba20c859d896407d2d31c5d2cf372dd6b5e71b9
Joe Collins <joe.collins121@gmail.com>
2021-01-07 11:05:07 -0700

    Add new example project using create-react-app

--- visits/index.js
+++ visits/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const redis = require('redis');
+const process = require('process');
 
 const app = express();
-const client = redis.createClient();
+const client = redis.createClient({
+  host: 'redis-server',
+  port: 6379
+});
 client.set('visits', 0);
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => {4
   client.get('visits', (err, visits) => {
     res.send("Num visits is: " + visits);
     client.set('visits', parseInt(visits) + 1);
